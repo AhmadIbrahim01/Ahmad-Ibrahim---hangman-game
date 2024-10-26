834a1342589b5186a84fbd29490794d81135a586
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-10-26 17:49:28 +0300

    Edit: game.js File

--- scripts/game.js
+++ scripts/game.js
@@ -1,25 +1,81 @@
-var words = ["programming", "coding", "bootcamp", "taha", "jane"]
+var words = ["programming", "codinga", "bootacamp", "taha", "jane"]
 
 var answer = document.getElementById("answer-section")
+var tries = 6
+
 
 var current_answer = words[(Math.floor(Math.random() * words.length))]
 console.log(current_answer)
 
-var current_answer_length = current_answer.length
-console.log(current_answer_length)
+// var current_answer_length = current_answer.length
+// console.log(current_answer_length)
 
 var dashes = []
+var current_answer_array = []
 
-for (var i = 0; i < current_answer_length; i++) {
+for (var i = 0; i < current_answer.length; i++) {
     // answer_array.push(current_answer[i])
     dashes.push("-")
+    current_answer_array.push(current_answer[i])
 }
 console.log(dashes)
+console.log(current_answer_array)
 
 
 // Displaying the array as an integer in the answer section
 var string_dashes = ""
-for (var i = 0; i < dashes.length; i++) {
-    string_dashes += dashes[i]
+function stringDashes(){
+    for (var i = 0; i < dashes.length; i++) {
+        string_dashes += dashes[i]
+    }
 }
+stringDashes()
 answer.innerHTML = string_dashes
+function display(){
+    answer.innerHTML = dashes.toLocaleString()
+}
+display()
+// function deleteOldAnswer(){
+//     answer.innerHTML = ""
+// }
+
+
+// Select all elements with the class "letter"
+var letters = document.querySelectorAll('.letter');
+
+// Add an event listener to each letter
+// letters.forEach(letter => {
+//   letter.addEventListener('click', () => {
+//     alert(`You clicked on: ${letter.textContent}`);
+//   });
+// });
+
+letters.forEach(letter => {
+  letter.addEventListener('click', () => {
+    var current_letter = letter.innerHTML.toLowerCase()
+    var flag = false
+    for (var i = 0; i < dashes.length; i++) {
+        if (current_answer_array[i] == current_letter && dashes.includes("-")){
+            dashes[i] = current_letter
+            console.log(dashes)
+            console.log(current_letter)
+            display()
+            flag = true
+        }
+        if(dashes.includes("-") == 0){
+            alert(`YOU WIN THE WORD IS ${current_answer}`)
+            flag = true
+            break
+        }
+    }
+
+    if (flag == false){
+        alert("not there")
+        tries -= 1
+        hangMan()
+    }
+
+
+  });
+});
+
